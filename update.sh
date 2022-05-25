a8287fef458fbb7e3df9922ba25e0fe90f14baf6
gamer191 <83270075+gamer191@users.noreply.github.com>
2022-05-26 00:58:15 +1000

    fix workflow

--- update.sh
+++ update.sh
@@ -4,7 +4,7 @@ rm -rf source_tmp
 curl https://opensource.truthsocial.com/mastodon-current.zip > mastodon-current.zip
 unzip mastodon-current.zip -d source_tmp
 
-mv source_tmp/truth_social_open_source_* source
+mv source_tmp/opensource source
 
 rm -rf source_tmp
 rm mastodon-current.zip
