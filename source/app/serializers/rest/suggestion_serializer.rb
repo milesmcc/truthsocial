e68610b44537cb0ea3a1385c288484c5c326fe68
R. Miles McCain <github@sendmiles.email>
2022-11-17 14:05:11 +0000

    Update source

--- source/app/serializers/rest/suggestion_serializer.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class REST::SuggestionSerializer < ActiveModel::Serializer
-  attributes :source
-
-  has_one :account, serializer: REST::AccountSerializer
-end
--- source/db/migrate/20220525140644_add_unauth_visibility_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddUnauthVisibilityToUsers < ActiveRecord::Migration[6.1]
-  disable_ddl_transaction!
-  def change
-    add_column :users, :unauth_visibility, :boolean
-  end
-end
