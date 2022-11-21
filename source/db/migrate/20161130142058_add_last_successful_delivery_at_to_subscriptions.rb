d6c4e5d3c4402aea883825988928c576616525e4
R. Miles McCain <github@sendmiles.email>
2022-11-21 11:04:56 +0000

    Update source

--- source/db/migrate/20161130142058_add_last_successful_delivery_at_to_subscriptions.rb
+++
@@ -1,5 +0,0 @@
-class AddLastSuccessfulDeliveryAtToSubscriptions < ActiveRecord::Migration[5.0]
-  def change
-    add_column :subscriptions, :last_successful_delivery_at, :datetime, null: true, default: nil
-  end
-end
--- source/db/migrate/20200614002136_add_sensitized_to_accounts.rb
+++
@@ -1,5 +0,0 @@
-class AddSensitizedToAccounts < ActiveRecord::Migration[5.2]
-  def change
-    add_column :accounts, :sensitized_at, :datetime
-  end
-end
