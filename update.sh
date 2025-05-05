68669832c3403faf8cbf0d672a8b3cc6cecd0b9b
gamer191 <83270075+gamer191@users.noreply.github.com>
2025-05-05 13:54:09 +1000

    Install default dependencies for future-proofing

--- update.sh
+++ update.sh
@@ -1,7 +1,7 @@
 rm -rf source
 rm -rf source_tmp
 
-pipx install yt-dlp[curl_cffi]
+pipx install yt-dlp[default,curl_cffi]
 yt-dlp --impersonate chrome --force-ipv4 https://opensource.truthsocial.com/mastodon-current.zip -o "mastodon-current.zip"||yt-dlp --impersonate chrome --force-ipv6 https://opensource.truthsocial.com/mastodon-current.zip -o "mastodon-current.zip"
 unzip mastodon-current.zip -d source_tmp
 
